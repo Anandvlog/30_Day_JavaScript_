825e7adf0aff06e2590a8cdd1765ccb85015793e
Anand Chauhan <71122607+Anandvlog@users.noreply.github.com>
2021-06-20 11:19:49 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,11 @@
+console.time("this is my codepan")
+console.warn('hello world');
+console.log('hello world');
+console.log([34,2,1,2]);
+console.log(34+2);
+console.log(true);
+// table
+console.table({anand: 'this', marks:30});
+// console.clear();
+console.timeEnd("this is my codepan")
+console.error("this is an error")
